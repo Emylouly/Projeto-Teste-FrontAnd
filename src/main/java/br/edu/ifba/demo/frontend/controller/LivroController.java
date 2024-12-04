0b254ce2a98e0bbdda4fbae54f3cf0379d77bd89
Emylouly <emilylouisemika@gmail.com>
2024-12-04 16:25:09 -0300

    Update LivroController.java

--- src/main/java/br/edu/ifba/demo/frontend/controller/LivroController.java
+++ src/main/java/br/edu/ifba/demo/frontend/controller/LivroController.java
@@ -3,6 +3,7 @@ package br.edu.ifba.demo.frontend.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.ifba.demo.frontend.service.LivroService;
@@ -20,6 +21,21 @@ public class LivroController {
         model.setViewName("livro");
         return model;
     }
+
+    @GetMapping("/livro/buscar/{id}")
+public ModelAndView buscarLivroPorId(@PathVariable("id") Long id) {
+    ModelAndView model = new ModelAndView();
+    try {
+        // Chama o serviço para buscar o livro pelo ID
+        model.addObject("livro", livroService.getById(id));
+        model.setViewName("detalhesLivro"); // Nome da página de detalhes do livro
+    } catch (Exception e) {
+        model.addObject("erro", "Livro não encontrado para o ID: " + id);
+        model.setViewName("erro");
+    }
+    return model;
+}
+
     
     
 }
